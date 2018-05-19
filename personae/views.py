07d8112dd02583a410c105c141e6eb75fabd3476
IcarusCodes <stoian.alex94@yahoo.com>
2018-05-19 15:25:33 +0300

    Covered the main logic of the website, including contact and Home

--- personae/views.py
+++ personae/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personae/home.html')
+
+def contact(request):
+    return render(request, 'personae/basic.html', {'content':['If you would like to contact me, please email me', 'stoian.alex94@yahoo.com']})
