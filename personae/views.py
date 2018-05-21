56c95ca40e17530ebd81c2d5a8fe7c0d58637484
IcarusCodes <stoian.alex94@yahoo.com>
2018-05-22 00:24:42 +0300

    Hipster Ipsum text

--- personae/views.py
+++ personae/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'personae/home.html')
 
 def contact(request):
-    return render(request, 'personae/basic.html', {'content':['If you would like to contact me, please email me', 'stoian.alex94@yahoo.com']})
+    return render(request, 'personae/basic.html', {'content':['Do franzen DIY butcher ut cred excepteur roof party, consectetur fugiat meh cardigan in. Put a bird on it pinterest lorem, scenester pok pok schlitz banh mi bespoke crucifix cred culpa food truck vaporware sustainable.']})
