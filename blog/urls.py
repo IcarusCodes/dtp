895ff2d74a7e472d381603ab8dd55108e7e1e63a
IcarusCodes <stoian.alex94@yahoo.com>
2018-05-23 23:00:12 +0300

    Added admin superuser.
    Blogs now display properly.
    
    Each blog post gets its own url.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog.models import Post
 
 
 urlpatterns = [
-    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html"))
+    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")),
+    path('<int:pk>/', DetailView.as_view(model=Post, template_name='blog/post.html'))
 ]
